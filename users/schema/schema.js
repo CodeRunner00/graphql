a301229f7539407a9b65eb94cb9654ed0ef4cc8e
Eric Forsell <Eric.Forsell@vitaminshoppe.com>
2019-02-09 12:03:16 -0500

    Add server

---
+++ users/schema/schema.js
@@ -0,0 +1,41 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+
+const {
+	GraphQLObjectType,
+	GraphQLString,
+	GraphQLInt,
+	GraphQLSchema
+} = graphql;
+
+const users = [
+	{ id: '23', firtsName: 'Bill', age: 20},
+	{ id: '47', firstName: 'Samantha', age 21}
+];
+
+const UserType = new GraphQLObjectType({
+	name: 'User',
+	fields: {
+		id: { type: GraphQLString },
+			firstName: { type: GraphQLString },
+			age: { type: GraphQLInt }
+	}
+});
+
+const RootQuery = new GraphQALObjectType({
+	name: 'RootQueryType',
+	fields: {
+		user: {
+			type: UserType,
+			args { id: { type: GraphQLString }},
+			resolve(parentValue, args) {
+				return _.find(users, { id: args.id });
+			}
+		}
+	}
+});
+
+module.exports = new GraphQLSchema({
+	query: RootQuery
+});
